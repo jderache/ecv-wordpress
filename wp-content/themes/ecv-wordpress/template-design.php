4802a5a33dc79953dc60913419d36ab44d229d84
jderache <contact@julienderache.fr>
2023-11-09 10:06:52 +0100

    style for btn primary

---
+++ wp-content/themes/ecv-wordpress/template-design.php
@@ -0,0 +1,17 @@
+<?php // Template Name: Page design 
+get_header(); ?>
+
+<section class="tattoo-designs">
+  <div class="gallery">
+    
+  </div>
+</section>
+
+<?php get_footer(); ?>
+
+
+  
+</section>
+
+
+
