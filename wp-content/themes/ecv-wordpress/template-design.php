fc9163bfe39d8d2c108f61cf6f0083353ab6aabd
jderache <contact@julienderache.fr>
2023-11-09 10:44:22 +0100

    ajout galerie sur page design

--- wp-content/themes/ecv-wordpress/template-design.php
+++ wp-content/themes/ecv-wordpress/template-design.php
@@ -1,17 +1,25 @@
-<?php // Template Name: Page design 
-get_header(); ?>
+<?php 
+/*
+  Template Name: Page design
+*/
 
-<section class="tattoo-designs">
-  <div class="gallery">
-    
-  </div>
-</section>
+  get_header();
+?>
+<?php 
+  $images = get_field( 'galerie' );
+  $size = 'full'; // (thumbnail, medium, large, full or custom size)
+  if( $images ): 
+?>
+  <ul class="acf-gallery">
+    <?php foreach( $images as $image_id ): ?>
+        <li>
+            <a href="<?php echo wp_get_attachment_url( $image_id ); ?>">
+              <?php echo wp_get_attachment_image( $image_id, $size ); ?>
+              </a>
+        </li>
+      <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
 
-<?php get_footer(); ?>
-
-
-  
-</section>
-
-
-
+<?php 
+  get_footer();
